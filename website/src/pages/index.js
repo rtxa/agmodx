88191f06546e4b7d4361aa1a6577f5df0119fbbe
rtxa <usertxa@gmail.com>
2024-08-31 01:40:19 -0300

    Docs: Include changelog in the site
    Instead of redirecting to an external site

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -26,7 +26,7 @@ function HomepageHeader() {
             <InlineIcon icon="lucide:file-text" height="1.5rem" style={{ verticalAlign: 'text-bottom' }} /> Read the docs
           </Link>
         </div>
-        Latest version: <b>Beta 2.5.2</b> — <a target="_blank" style={{ color: '#5a98fe' }} href="https://github.com/rtxa/agmodx/blob/master/CHANGELOG.md">What's new?<IconExternalLink /></a>
+        Latest version: <b>Beta 2.5.2</b> — <Link to='/changelog'>What's new?</Link>
       </div>
     </header>
   );
